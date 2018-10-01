bebb4f9ea497c1dba8edb43bef62a6f047e6dabc
AlezanaAbrego <alezana@outlook.es>
2018-10-01 13:01:24 -0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose.js');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 
 
@@ -11,39 +12,69 @@ var app = express();
 
 app.use(bodyParser.json());
 
-app.post('/todos', (req,res) =>{
+app.post('/todos', (req, res) => {
 
     var todo = new Todo({
         text: req.body.text
     });
 
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    },(e)=>{
+    }, (e) => {
         res
-        .status(400)
-        .send(e);
+            .status(400)
+            .send(e);
     });
 
     console.log(req.body);
 });
 
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
         res.send({
             todos
         });
-    },(e)=>{
-res.status(400).send(e);
+    }, (e) => {
+        res.status(400).send(e);
     })
 });
 
-app.listen(3000,()=>{
+//get /todos/123432
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+
+   res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+    //Valid id using isValid
+    //404 - send back empty send
+    //findById
+    //succedd
+    //if todo - send it back
+
+    //res.send(req.params);
+});
+
+app.listen(3000, () => {
     console.log('Started on port 3000');
 })
 
 
-module.exports = {app};
+
+
+module.exports = { app };
 
 
 
